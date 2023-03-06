14c1ebc3b61b61bc814b8c1104d289db334d22ee
sadikov16 <sodiqovu880@gmail.com>
2023-03-06 20:30:12 +0500

    Main

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const resultFootEl = document.querySelector(".foot")
 const resultBicycleEl = document.querySelector(".bicycle")
 const resultCarEl = document.querySelector(".car")
 const resultPlaneEl = document.querySelector(".plane")
+const ElBody = document.querySelector("body")
+const ElModeBtn = document.querySelector("#ModeBtn")
+
+ElModeBtn.addEventListener("click", event => {
+    if(ElModeBtn.textContent = "Dark"){
+        ElBody.classList.add("Light");
+        ElBody.classList.remove("Dark");
+        ElModeBtn.textContent = "Light";
+    }else if(ElModeBtn.textContent = "Light"){
+        ElBody.classList.add("Dark");
+        ElBody.classList.remove("Light");
+        ElModeBtn.textContent = "Dark";
+    }
+})
+
 formEl.addEventListener("submit", function(evt){
     evt.preventDefault();
     myFunction(inputEl.value);
@@ -14,7 +29,7 @@ let myFunction = function(value){
     const byBicycle = value / 20.1
     const bycar = value / 70
     const byplane = value / 800
-
+    
     resultFootEl.textContent = onFoot.toFixed(2) + " Soat"
     resultBicycleEl.textContent = byBicycle.toFixed(2)+ " Soat"
     resultCarEl.textContent = bycar.toFixed(2)+ " Soat"
